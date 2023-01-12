8b24c5bdab61eb8fd0318b4e56023b837826eeae
Víctor <vrmmb@hotmail.com>
2023-01-12 19:41:37 +0100

    Added to HTTP_CLIENT_CONFIGURATION: Method json. Method form

--- src/Common/Adapter/HttpClientConfiguration/HTTP_CLIENT_CONFIGURATION.php
+++ src/Common/Adapter/HttpClientConfiguration/HTTP_CLIENT_CONFIGURATION.php
@@ -4,19 +4,58 @@ declare(strict_types=1);
 
 namespace Common\Adapter\HttpClientConfiguration;
 
-class HTTP_CLIENT_CONFIGURATION
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
+
+final class HTTP_CLIENT_CONFIGURATION
 {
     public const API_DOMAIN = 'http://orderlist.api';
     public const CLIENT_DOMAIN = 'http://orderlist.client';
     public const XDEBUG_VAR = 'XDEBUG_SESSION=VSCODE';
 
-    public static function json(array $data): array
+    public static function json(array $data = null, string $tokenSession = null): array
+    {
+        $json = self::getConfiguration();
+
+        null === $data ?: $json['json'] = $data;
+        null === $tokenSession ?: $json['auth_bearer'] = $tokenSession;
+
+        return $json;
+    }
+
+    public static function form(array $data, array $files = [], string $tokenSession = null): array
+    {
+        $form = self::getConfiguration();
+        null === $tokenSession ?: $form['auth_bearer'] = $tokenSession;
+
+        $files = array_filter($files);
+        $files = array_map(self::loadFiles(...), $files);
+        $data = array_map(fn (mixed $value) => (string) $value, $data);
+        $data = array_merge($data, $files);
+        $formdData = new FormDataPart($data);
+
+        $form['body'] = $formdData->toIterable();
+        $form['headers'] = $formdData->getPreparedHeaders()->toArray();
+
+        return $form;
+    }
+
+    private static function loadFiles(UploadedFile|null $file): DataPart|null
+    {
+        if (null === $file) {
+            return null;
+        }
+
+        return DataPart::fromPath($file->getPathname());
+    }
+
+    private static function getConfiguration(): array
     {
         return [
             'proxy' => 'http://proxy:80',
             'verify_peer' => false,
             'verify_host' => false,
-            'json' => $data
         ];
     }
 }
