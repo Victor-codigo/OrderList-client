9f69a4222db6db73eaafd5ec1a0c91f4c00444ef
Víctor <victorrmmb@hotmail.com>
2023-04-18 12:07:02 +0200

    fix: :bug: Fixed group list, in modal, now shows the name of the group to delete. Fixed GroupRemoveComponent tranlation file group_name placeholder now is right. Fixed GroupUsersListComponent+intl-icu.es.yaml file name.

--- src/Twig/Components/Group/GroupList/ListItem/GroupListItem_controller.js
+++ src/Twig/Components/Group/GroupList/ListItem/GroupListItem_controller.js
@@ -3,12 +3,19 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        this.groupId = this.element.querySelector('[data-js-group-id]').value;
+        this.groupIdInput = this.element.querySelector('[data-js-group-id]');
+        this.groupId = this.groupIdInput.value;
+        this.groupName = this.groupIdInput.dataset.groupName;
         this.groupRemoveButton = this.element.querySelector('[data-js-group-remove-button]');
         this.removeClickListener = this.groupRemoveButton.addEventListener('click', this.groupRemove.bind(this));
     }
 
     groupRemove() {
-        this.dispatch('onGroupRemoveEvent', { detail: { groupId: this.groupId } });
+        this.dispatch('onGroupRemoveEvent', {
+            detail: {
+                groupId: this.groupId,
+                groupName: this.groupName
+            }
+        });
     }
 }
--- src/Twig/Components/Group/GroupRemove/GroupRemove_controller.js
+++ src/Twig/Components/Group/GroupRemove/GroupRemove_controller.js
@@ -6,7 +6,16 @@ export default class extends Controller {
         this.groupRemoveId = this.element.querySelector('[data-js-group-id]');
     }
 
-    setGroupIdToRemove({ detail: { groupId } }) {
+    setGroupIdToRemove({ detail: { groupId, groupName } }) {
         this.groupRemoveId.value = groupId;
+
+        console.log(groupName);
+        this.setGroupNameInMessage(groupName);
+    }
+
+    setGroupNameInMessage(groupName) {
+        let messageParagraph = this.element.querySelector('[data-js-message]');
+
+        messageParagraph.innerHTML = messageParagraph.dataset.message.replace('#GROUP_NAME#', groupName);
     }
 }
