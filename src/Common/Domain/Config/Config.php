1385bed8e4d501db82c66b4dfd9749df706ceefd
Víctor <victorrmmb@hotmail.com>
2023-11-10 17:32:52 +0100

    feat: :sparkles: Added Config class, to add app configuration

---
+++ src/Common/Domain/Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Domain\Config;
+
+use Common\Adapter\HttpClientConfiguration\HTTP_CLIENT_CONFIGURATION;
+
+class Config
+{
+    private const CLIENT_DOMAIN = 'http://orderlist.client';
+
+    public const SHOP_IMAGE_NO_IMAGE_PUBLIC_PATH_200_200 = self::CLIENT_DOMAIN.'/assets/images/shop/shop-no-image-200x200.svg';
+
+    public const API_IMAGES_SHOP_PATH = HTTP_CLIENT_CONFIGURATION::API_DOMAIN.'/assets/img/shops';
+}
