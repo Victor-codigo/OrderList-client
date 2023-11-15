e01e9851a17732099a2f391de08d89d20597c3a6
Víctor <victorrmmb@hotmail.com>
2023-11-15 17:32:25 +0100

    refactor: :recycle: Added methods setImage and handleOnSetImageEvent. Moved images remove-icon and undo-icon to public images path
    Added method handleOnSetImageEvent, to handle setImageEvent data. Changed the way of importing undo and remove icons. now imports are made with include statement

--- src/Twig/Components/Controls/ImageAvatar/ImageAvatar_Component.js
+++ src/Twig/Components/Controls/ImageAvatar/ImageAvatar_Component.js
@@ -5,17 +5,29 @@ export default class extends Controller {
 
     connect() {
         this.image = this.element.querySelector('[data-js-image]');
-        this.imageUrl = this.image.src;
-
         this.buttonImageRemove = this.element.querySelector('[data-js-button-remove]');
         this.buttonImageRemove.addEventListener('click', this.removeImage.bind(this));
 
         this.buttonImageRemoveUndo = this.element.querySelector('[data-js-button-remove-undo]');
         this.buttonImageRemoveUndo.addEventListener('click', this.removeImageUndo.bind(this));
 
+        this.setImage(this.image.src);
+    }
+
+    setImage(imageUrl) {
+        this.image.src = imageUrl;
+        this.imageUrl = this.image.src;
+
         if (this.imageUrl === this.image.dataset.noAvatar) {
             this.buttonImageRemove.classList.add('d-none');
+            this.buttonImageRemoveUndo.classList.add('d-none');
+
+            return;
         }
+
+        this.buttonImageRemoveUndo.classList.add('d-none');
+        this.buttonImageRemove.classList.remove('d-none');
+
     }
 
     removeImage() {
@@ -32,5 +44,9 @@ export default class extends Controller {
         this.buttonImageRemove.classList.remove('d-none');
 
         this.dispatch('imageRemovedUndo', { detail: { imageRemove: false } });
+    }
+
+    handleOnSetImageEvent({ detail: { content } }) {
+        this.setImage(content.shopImage);
     }
 }
