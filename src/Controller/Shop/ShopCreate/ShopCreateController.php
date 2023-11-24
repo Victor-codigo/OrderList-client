7722b49cb581940c1f9f3884fabd9c0ea15b87d0
Víctor <victorrmmb@hotmail.com>
2023-11-24 13:36:57 +0100

    refactor: :recycle: Refactored ShopCreateController, to use class ControllerUrlRefererRedirect

--- src/Controller/Shop/ShopCreate/ShopCreateController.php
+++ src/Controller/Shop/ShopCreate/ShopCreateController.php
@@ -6,21 +6,18 @@ use App\Controller\Request\RequestDto;
 use App\Form\Shop\ShopCreate\SHOP_CREATE_FORM_FIELDS;
 use App\Form\Shop\ShopCreate\ShopCreateForm;
 use App\Twig\Components\Shop\ShopCreate\ShopCreateComponent;
-use Common\Adapter\Events\Exceptions\RequestRefererException;
-use Common\Domain\Config\Config;
+use Common\Domain\ControllerUrlRefererRedirect\ControllerUrlRefererRedirect;
 use Common\Domain\Ports\Endpoints\EndpointsInterface;
-use Common\Domain\Ports\FlashBag\FlashBagInterface;
 use Common\Domain\Ports\Form\FormFactoryInterface;
 use Common\Domain\Ports\Form\FormInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(
     path: '{_locale}/shop/{group_name}/create',
     name: 'shop_create',
-    methods: ['GET', 'POST'],
+    methods: ['POST'],
     requirements: [
         '_locale' => 'en|es',
     ]
@@ -30,21 +27,26 @@ class ShopCreateController extends AbstractController
     public function __construct(
         private FormFactoryInterface $formFactory,
         private EndpointsInterface $endpoints,
-        private FlashBagInterface $sessionFlashBag,
+        private ControllerUrlRefererRedirect $controllerUrlRefererRedirect,
         private ShopCreateComponent $shopCreateComponent
     ) {
     }
 
     public function __invoke(RequestDto $requestDto): Response
     {
-        $this->validateReferer($requestDto);
+        $this->controllerUrlRefererRedirect->validateReferer($requestDto->requestReferer);
         $shopCreateForm = $this->formFactory->create(new ShopCreateForm(), $requestDto->request);
 
         if ($shopCreateForm->isSubmitted() && $shopCreateForm->isValid()) {
             $this->formValid($shopCreateForm, $requestDto->groupData->id, $requestDto->tokenSession);
         }
 
-        return $this->createRedirectToRoute($requestDto, $shopCreateForm);
+        return $this->controllerUrlRefererRedirect->createRedirectToRoute(
+            $requestDto->requestReferer->routeName,
+            $requestDto->requestReferer->params,
+            [$this->shopCreateComponent->loadValidationOkTranslation()],
+            $this->shopCreateComponent->loadErrorsTranslation($shopCreateForm->getErrors())
+        );
     }
 
     private function formValid(FormInterface $form, string $groupId, string $tokenSession): void
@@ -59,35 +61,6 @@ class ShopCreateController extends AbstractController
 
         foreach ($responseData['errors'] as $error => $errorDescription) {
             $form->addError($error, $errorDescription);
-        }
-    }
-
-    private function createRedirectToRoute(RequestDto $requestDto, FormInterface $form): RedirectResponse
-    {
-        $formErrorsMessages = $this->shopCreateComponent->loadErrorsTranslation($form->getErrors());
-        if (empty($formErrorsMessages)) {
-            $this->sessionFlashBag->add(
-                $form->getFormName().Config::FLASH_BAG_FORM_NAME_SUFFIX_MESSAGE_OK,
-                $this->shopCreateComponent->loadValidationOkTranslation()
-            );
-        }
-
-        array_map(
-            fn (string $error) => $this->sessionFlashBag->add($form->getFormName().Config::FLASH_BAG_FORM_NAME_SUFFIX_MESSAGE_ERROR, $error),
-            $formErrorsMessages
-        );
-
-        return $this->redirectToRoute(
-            $requestDto->requestReferer->routeName,
-            $requestDto->requestReferer->params,
-            Response::HTTP_MOVED_PERMANENTLY
-        );
-    }
-
-    private function validateReferer(RequestDto $requestDto): void
-    {
-        if (null === $requestDto->requestReferer) {
-            throw RequestRefererException::fromMessage('Request referer not valid');
         }
     }
 }
