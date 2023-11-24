424d58a3491b48e8a892bbaece55121f6a637e0e
Víctor <victorrmmb@hotmail.com>
2023-11-24 13:37:23 +0100

    refactor: :recycle: Refactored ShopModifyController, to use class ControllerUrlRefererRedirect

--- src/Controller/Shop/ShopModify/ShopModifyController.php
+++ src/Controller/Shop/ShopModify/ShopModifyController.php
@@ -3,11 +3,10 @@
 namespace App\Controller\Shop\ShopModify;
 
 use App\Controller\Request\RequestDto;
-use App\Controller\Request\Response\ShopDataResponse;
 use App\Form\Shop\ShopModify\SHOP_MODIFY_FORM_FIELDS;
 use App\Form\Shop\ShopModify\ShopModifyForm;
-use App\Twig\Components\Shop\ShopModify\ShopModifyComponentDto;
-use Common\Domain\Config\Config;
+use App\Twig\Components\Shop\ShopModify\ShopModifyComponent;
+use Common\Domain\ControllerUrlRefererRedirect\ControllerUrlRefererRedirect;
 use Common\Domain\Ports\Endpoints\EndpointsInterface;
 use Common\Domain\Ports\Form\FormFactoryInterface;
 use Common\Domain\Ports\Form\FormInterface;
@@ -18,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(
     path: '{_locale}/shop/{group_name}/modify/{shop_name}',
     name: 'shop_modify',
-    methods: ['GET', 'POST'],
+    methods: ['POST'],
     requirements: [
         '_locale' => 'en|es',
     ]
@@ -28,28 +27,25 @@ class ShopModifyController extends AbstractController
     public function __construct(
         private FormFactoryInterface $formFactory,
         private EndpointsInterface $endpoints,
+        private ShopModifyComponent $shopModifyComponent,
+        private ControllerUrlRefererRedirect $controllerUrlRefererRedirect
     ) {
     }
 
     public function __invoke(RequestDto $requestDto): Response
     {
+        $this->controllerUrlRefererRedirect->validateReferer($requestDto->requestReferer);
         $shopModifyForm = $this->formFactory->create(new ShopModifyForm(), $requestDto->request);
 
-        $validated = false;
         if ($shopModifyForm->isSubmitted() && $shopModifyForm->isValid()) {
-            $validated = true;
             $this->formValid($shopModifyForm, $requestDto->groupData->id, $requestDto->shopData->id, $requestDto->tokenSession);
-
-            if (!$shopModifyForm->hasErrors()) {
-                return $this->redirectToRoute('shop_modify', [
-                    'group_name' => $this->endpoints->encodeUrl($requestDto->groupData->name),
-                    'shop_name' => $this->endpoints->encodeUrl($shopModifyForm->getFieldData(SHOP_MODIFY_FORM_FIELDS::NAME)),
-                ]);
-            }
         }
 
-        return $this->renderTemplate(
-            $this->createShopModifyComponent($shopModifyForm, $requestDto->shopData, $validated)
+        return $this->controllerUrlRefererRedirect->createRedirectToRoute(
+            $requestDto->requestReferer->routeName,
+            $requestDto->requestReferer->params,
+            [$this->shopModifyComponent->loadValidationOkTranslation()],
+            $this->shopModifyComponent->loadErrorsTranslation($shopModifyForm->getErrors())
         );
     }
 
@@ -68,27 +64,5 @@ class ShopModifyController extends AbstractController
         foreach ($responseData['errors'] as $error => $errorDescription) {
             $form->addError($error, $errorDescription);
         }
-    }
-
-    private function createShopModifyComponent(FormInterface $form, ShopDataResponse $shopData, bool $validated): ShopModifyComponentDto
-    {
-        return new ShopModifyComponentDto(
-            $form->getErrors(),
-            $shopData->name,
-            $shopData->description,
-            null === $shopData->image
-                ? Config::SHOP_IMAGE_NO_IMAGE_PUBLIC_PATH_200_200
-                : Config::API_IMAGES_SHOP_PATH."/{$shopData->image}",
-            Config::SHOP_IMAGE_NO_IMAGE_PUBLIC_PATH_200_200,
-            $form->getCsrfToken(),
-            $validated
-        );
-    }
-
-    private function renderTemplate(ShopModifyComponentDto $shopModifyComponentDto): Response
-    {
-        return $this->render('shop/shop_modify/index.html.twig', [
-            'shopModifyComponent' => $shopModifyComponentDto,
-        ]);
     }
 }
