71fa296a396c507609f97f4e72549a5517b3579f
Victor <victorrmmb@hotmail.com>
2023-12-14 17:43:17 +0100

    refactor: :recycle: Added method TwigComponent::setTranslationDomainName, to allow Twig component to chose the domain name for translation

--- src/Twig/Components/TwigComponent.php
+++ src/Twig/Components/TwigComponent.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Twig\Components;
 
-use LogicException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -12,6 +11,7 @@ abstract class TwigComponent
 {
     abstract protected static function getComponentName(): string;
 
+    private string $translationDomainName;
     protected TranslatorInterface $translator;
     protected RequestStack $request;
     public TwigComponentDtoInterface $data;
@@ -20,28 +20,34 @@ abstract class TwigComponent
     {
         $this->translator = $translator;
         $this->request = $request;
+        $this->setTranslationDomainName($this->getComponentName());
     }
 
     protected function translate(string $id, array $params = []): string
     {
-        return $this->translator->trans($id, $params, $this->getComponentName());
+        return $this->translator->trans($id, $params, $this->translationDomainName);
     }
 
     /**
-     * @throws LogicException
+     * @throws \LogicException
      */
     protected function loadFromSession(): void
     {
-        $data = $this->request->getSession()->get($this->getComponentName());
+        $data = $this->request->getSession()->get($this->translationDomainName);
 
-        if ($data===null) {
-            throw new LogicException('Could not find ['.$this->getComponentName().'] in session variables');
+        if (null === $data) {
+            throw new \LogicException('Could not find ['.$this->getComponentName().'] in session variables');
         }
 
         if (!$data instanceof TwigComponentDtoInterface) {
-            throw new LogicException('Session var ['.$this->getComponentName().'], is not an instance of ['.TwigComponentDtoInterface::class.']');
+            throw new \LogicException('Session var ['.$this->getComponentName().'], is not an instance of ['.TwigComponentDtoInterface::class.']');
         }
 
         $this->data = $data;
+    }
+
+    protected function setTranslationDomainName(string $domainName): void
+    {
+        $this->translationDomainName = $domainName;
     }
 }
