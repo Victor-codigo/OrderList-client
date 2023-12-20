dd5220061dd060c4f7149e2b193d143d4223b3b3
Victor <victorrmmb@hotmail.com>
2023-12-20 11:57:18 +0100

    feat: :sparkles: Created provisional ProductRemoveMultiForm

---
+++ src/Form/Product/ProductRemoveMulti/PRODUCT_REMOVE_MULTI_FORM_FIELDS.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Product\ProductRemoveMulti;
+
+class PRODUCT_REMOVE_MULTI_FORM_FIELDS
+{
+    public const FORM = 'product_remove_multi_form';
+    public const PRODUCTS_ID = 'products_id';
+    public const TOKEN = 'token';
+    public const SUBMIT = 'submit';
+}
