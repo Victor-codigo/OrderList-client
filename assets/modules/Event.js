efe797cf43e9d2a2988309879339160678197830
Victor <victorrmmb@hotmail.com>
2024-01-04 11:22:35 +0100

    refactor: :sparkles: Added to module Events, methods addEventListenerDelegate, and removeEventListenerDelegate

--- assets/modules/Event.js
+++ assets/modules/Event.js
@@ -3,3 +3,22 @@ export function dispatch(componentDispatcher, componentHandler, eventName, detai
 
     componentDispatcher.dispatchEvent(event);
 }
+
+export function addEventListenerDelegate({ element, elementDelegateSelector, eventName, callbackListener, eventOptions }) {
+    let delegateFunction = (event) => {
+        const elementTargetEvent = event.target.closest(elementDelegateSelector);
+
+        if (elementTargetEvent === null) {
+            return;
+        }
+
+        callbackListener(elementTargetEvent, event);
+    };
+
+    element.addEventListener(eventName, delegateFunction, eventOptions);
+}
+
+export function removeEventListenerDelegate(element, eventName) {
+    element.removeEventListener(eventName, delegateFunction);
+}
+
