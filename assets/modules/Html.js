7449e13e0cfc307a8c92d4b738c303d1b625d62d
Victor <victorrmmb@hotmail.com>
2024-01-17 12:38:47 +0100

    feat: :sparkles: Added module Html

---
+++ assets/modules/Html.js
@@ -0,0 +1,17 @@
+export function htmlEscape(string) {
+    return string
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&#039;');
+}
+
+export function htmlUnescape() {
+    return string
+        .replaceAll('&amp;', '&')
+        .replaceAll('&lt;', '<')
+        .replaceAll('&gt;', '>')
+        .replaceAll('&quot;', '"')
+        .replaceAll('&#039;', "'");
+}
