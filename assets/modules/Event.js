92929830f9fdee50494cd51dbc52fca898251b5c
Victor <victorrmmb@hotmail.com>
2024-01-17 12:38:16 +0100

    docs: :memo: Added documentation for module Event

--- assets/modules/Event.js
+++ assets/modules/Event.js
@@ -1,9 +1,25 @@
+/**
+ * @param {HTMLElement} componentDispatcher
+ * @param {string} componentHandler
+ * @param {string} eventName
+ * @param {*} detail
+ */
 export function dispatch(componentDispatcher, componentHandler, eventName, detail,) {
-    const event = new CustomEvent(`${componentHandler}:${eventName}`, detail);
+    const eventComponentName = componentHandler === "" ? "" : `${componentHandler}:`
+
+    const event = new CustomEvent(`${eventComponentName}${eventName}`, detail);
 
     componentDispatcher.dispatchEvent(event);
 }
 
+/**
+ * @param {Object} delegate
+ * @param {HTMLElement} delegate.element
+ * @param {string} delegate.elementDelegateSelector
+ * @param {string} delegate.eventName
+ * @param {callback} delegate.callbackListener
+ * @param {(boolean|AddEventListenerOptions|Array)} delegate.eventOptions
+ */
 export function addEventListenerDelegate({ element, elementDelegateSelector, eventName, callbackListener, eventOptions }) {
     let delegateFunction = (event) => {
         const elementTargetEvent = event.target.closest(elementDelegateSelector);
@@ -18,6 +34,10 @@ export function addEventListenerDelegate({ element, elementDelegateSelector, eve
     element.addEventListener(eventName, delegateFunction, eventOptions);
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} eventName
+ */
 export function removeEventListenerDelegate(element, eventName) {
     element.removeEventListener(eventName, delegateFunction);
 }
