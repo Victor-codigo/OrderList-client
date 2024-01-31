3bfb3af8fe325618a3946764f7fc56595ed821a5
Victor <victorrmmb@hotmail.com>
2024-01-31 11:16:17 +0100

    refactor: :recycle: Refactored ShopCreateController, to create new route for ajax creations

--- src/Controller/Shop/ShopCreate/ShopCreateController.php
+++ src/Controller/Shop/ShopCreate/ShopCreateController.php
@@ -10,13 +10,13 @@ use Common\Domain\ControllerUrlRefererRedirect\ControllerUrlRefererRedirect;
 use Common\Domain\Ports\Endpoints\EndpointsInterface;
 use Common\Domain\Ports\Form\FormFactoryInterface;
 use Common\Domain\Ports\Form\FormInterface;
+use Common\Domain\Response\RESPONSE_STATUS;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(
-    path: '{_locale}/{group_name}/shop/create',
-    name: 'shop_create',
     methods: ['POST'],
     requirements: [
         '_locale' => 'en|es',
@@ -32,14 +32,13 @@ class ShopCreateController extends AbstractController
     ) {
     }
 
-    public function __invoke(RequestDto $requestDto): Response
+    #[Route(
+        path: '{_locale}/{group_name}/shop/create',
+        name: 'shop_create'
+    )]
+    public function createShopHttp(RequestDto $requestDto): Response
     {
-        $this->controllerUrlRefererRedirect->validateReferer($requestDto->requestReferer);
-        $shopCreateForm = $this->formFactory->create(new ShopCreateForm(), $requestDto->request);
-
-        if ($shopCreateForm->isSubmitted() && $shopCreateForm->isValid()) {
-            $this->formValid($shopCreateForm, $requestDto->groupData->id, $requestDto->tokenSession);
-        }
+        ['form' => $shopCreateForm] = $this->shopCreateForm($requestDto);
 
         return $this->controllerUrlRefererRedirect->createRedirectToRoute(
             $requestDto->requestReferer->routeName,
@@ -50,11 +49,50 @@ class ShopCreateController extends AbstractController
         );
     }
 
-    private function formValid(FormInterface $form, string $groupId, string $tokenSession): void
+    #[Route(
+        path: 'ajax/{_locale}/{group_name}/shop/create',
+        name: 'shop_create_ajax',
+    )]
+    public function createShopAjax(RequestDto $requestDto): Response
+    {
+        ['form' => $shopCreateForm, 'responseData' => $responseData] = $this->shopCreateForm($requestDto);
+        $responseData['errors'] = $this->shopCreateComponent->loadErrorsTranslation(
+            $shopCreateForm->getErrors()
+        );
+
+        return new JsonResponse(
+            $responseData,
+            RESPONSE_STATUS::OK === $responseData['status'] ? Response::HTTP_CREATED : Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @return array<{
+     *  form: FormInterface,
+     *  responseData: array
+     * }>
+     */
+    public function shopCreateForm(RequestDto $requestDto): array
+    {
+        $this->controllerUrlRefererRedirect->validateReferer($requestDto->requestReferer);
+        $shopCreateForm = $this->formFactory->create(new ShopCreateForm(), $requestDto->request);
+        $shopCreatedResponseData = null;
+
+        if ($shopCreateForm->isSubmitted() && $shopCreateForm->isValid()) {
+            $shopCreatedResponseData = $this->shopCreateRequest($shopCreateForm, $requestDto->groupData->id, $requestDto->tokenSession);
+        }
+
+        return [
+            'form' => $shopCreateForm,
+            'responseData' => $shopCreatedResponseData,
+        ];
+    }
+
+    private function shopCreateRequest(FormInterface $form, string $groupId, string $tokenSession): array
     {
         $responseData = $this->endpoints->shopCreate(
             $groupId,
-            $form->getFieldData(SHOP_CREATE_FORM_FIELDS::NAME),
+            $form->getFieldData(SHOP_CREATE_FORM_FIELDS::NAME, ''),
             $form->getFieldData(SHOP_CREATE_FORM_FIELDS::DESCRIPTION),
             $form->getFieldData(SHOP_CREATE_FORM_FIELDS::IMAGE),
             $tokenSession
@@ -63,5 +101,7 @@ class ShopCreateController extends AbstractController
         foreach ($responseData['errors'] as $error => $errorDescription) {
             $form->addError($error, $errorDescription);
         }
+
+        return $responseData;
     }
 }
