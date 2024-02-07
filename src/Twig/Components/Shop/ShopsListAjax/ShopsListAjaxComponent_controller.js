c99ffb82ae4c9faf6dc7b5e0e0faa8be5b690dc7
Victor <victorrmmb@hotmail.com>
2024-02-07 16:40:30 +0100

    refactor: :recycle: Refactored js to make propertyes private. Adapt to module ModalManager.

--- src/Twig/Components/Shop/ShopsListAjax/ShopsListAjaxComponent_controller.js
+++ src/Twig/Components/Shop/ShopsListAjax/ShopsListAjaxComponent_controller.js
@@ -2,23 +2,86 @@ import { Controller } from '@hotwired/stimulus';
 import ListItems from 'App/modules/ListItems';
 import * as event from 'App/modules/Event';
 import * as communication from 'App/modules/ControllerCommunication';
+import ModalManager from 'App/modules/ModalManager/ModalManager';
+
+const LIST_ATTRIBUTE_SELECTOR = "data-js-list-shops";
+const LIST_ITEM_ATTRIBUTE_SELECTOR = "data-js-list-shops-item";
 
 /**
- * @event ShopsListAjaxComponent:shopSelected
  * @event PaginatorContentLoaderJsComponent:initialize
  */
 export default class extends Controller {
-    connect() {
-        this.modalBeforeAttributeId = null;
-        this.modalBeforeControllerEventHandler = null;
-        this.urlPathShopsImages = this.element.dataset.urlPathShopsImages;
-        this.urlNoShopsImage = this.element.dataset.urlNoShopsImage
-        this.shopImageTitle = this.element.dataset.shopImageTitle
-        this.shopsNotSelectable = [];
+    /**
+     * @type {ModalManager}
+     */
+    #modalManager;
 
-        this.paginatorContentLoaderJsComponent = this.element.querySelector('[data-controller="PaginatorContentLoaderJsComponent"]');
-        this.paginatorJsComponent = this.element.querySelector('[data-controller="PaginatorJsComponent"]');
-        this.backButtonTag = this.element.querySelector('[data-js-back-button]');
+    /**
+     * @type {string}
+     */
+    #urlPathShopsImages;
+
+    /**
+     * @type {string}
+     */
+    #urlNoShopsImage;
+
+    /**
+     * @type {string}
+     */
+    #shopImageTitle;
+
+    /**
+     * @type {string[]}
+     */
+    #shopsNotSelectable;
+
+    /**
+     * @type {HTMLElement}
+     */
+    #paginatorContentLoaderJsComponent;
+
+    /**
+     * @type {HTMLElement}
+     */
+    #paginatorJsComponent;
+
+    /**
+     * @type {HTMLButtonElement}
+     */
+    #backButtonTag;
+
+    /**
+     * @type {HTMLButtonElement}
+     */
+    #createShopButtonTag;
+
+    connect() {
+        this.#urlPathShopsImages = this.element.dataset.urlPathShopsImages;
+        this.#urlNoShopsImage = this.element.dataset.urlNoShopsImage
+        this.#shopImageTitle = this.element.dataset.shopImageTitle
+        this.#shopsNotSelectable = [];
+
+        this.#paginatorContentLoaderJsComponent = this.element.querySelector('[data-controller="PaginatorContentLoaderJsComponent"]');
+        this.#paginatorJsComponent = this.element.querySelector('[data-controller="PaginatorJsComponent"]');
+        this.#backButtonTag = this.element.querySelector('[data-js-back-button]');
+        this.#createShopButtonTag = this.element.querySelector('[data-js-create-shop-button]');
+
+        this.#backButtonTag.addEventListener('click', this.#openModalBefore.bind(this));
+        this.#createShopButtonTag.addEventListener('click', this.#openModalCreateShop.bind(this))
+        event.addEventListenerDelegate({
+            element: this.element,
+            elementDelegateSelector: '[data-js-list-shops-item]',
+            eventName: 'click',
+            callbackListener: this.#openModalShopSelected.bind(this),
+            eventOptions: {}
+        });
+    }
+
+    disconnect() {
+        this.#backButtonTag.removeEventListener('click', this.#openModalBefore);
+        this.#createShopButtonTag.removeEventListener('click', this.#openModalCreateShop);
+        event.removeEventListenerDelegate(this.element, 'click', this.#openModalShopSelected);
     }
 
     /**
@@ -32,11 +95,11 @@ export default class extends Controller {
                     id: shopData.id,
                     name: shopData.name,
                     image: {
-                        title: this.shopImageTitle.replace('{shop_name}', shopData.name),
-                        alt: this.shopImageTitle.replace('{shop_name}', shopData.name),
+                        title: this.#shopImageTitle.replace('{shop_name}', shopData.name),
+                        alt: this.#shopImageTitle.replace('{shop_name}', shopData.name),
                         src: shopData.image === null
-                            ? this.urlNoShopsImage
-                            : `${this.urlPathShopsImages}/${shopData.image}`
+                            ? this.#urlNoShopsImage
+                            : `${this.#urlPathShopsImages}/${shopData.image}`
                     },
                     data: {
                         id: shopData.id,
@@ -44,14 +107,13 @@ export default class extends Controller {
                     },
                     item: {
                         htmlAttributes: {
-                            "data-bs-target": '#' + this.modalBeforeAttributeId,
-                            "data-bs-toggle": "modal",
+                            [LIST_ITEM_ATTRIBUTE_SELECTOR]: ""
                         },
                         cssClasses: []
                     },
                 };
 
-                if (this.shopsNotSelectable.includes(shopData.id)) {
+                if (this.#shopsNotSelectable.includes(shopData.id)) {
                     itemData.item.cssClasses.push("disabled");
                 }
 
@@ -60,46 +122,44 @@ export default class extends Controller {
 
         this.#sendMessagePagesTotalToPaginatorJsComponent(responseData.pages_total);
 
-        return new ListItems({}, itemsData);
+        const listData = {
+            htmlAttributes: {
+                [LIST_ATTRIBUTE_SELECTOR]: ""
+            }
+        };
+
+        return new ListItems(listData, itemsData);
     }
 
     /**
-     * @param {HTMLElement} container
-     * @param {Event} eventData
+     * @param {Event} event
      */
-    #postResponseManageCallback(container, eventData) {
-        const list = container.querySelector('ul');
+    #openModalBefore(event) {
+        this.#modalManager.openModalBefore();
+    }
 
-        event.addEventListenerDelegate({
-            element: list,
-            elementDelegateSelector: 'li',
-            eventName: 'click',
-            callbackListener: this.handleMessageShopSelected.bind(this),
-            eventOptions: {}
+    /**
+     * @param {HTMLElement} relatedTarget
+     * @param {Event} event
+     */
+    #openModalShopSelected(relatedTarget, event) {
+        const itemData = JSON.parse(relatedTarget.dataset.data);
+        this.#modalManager.openNewModal(this.#modalManager.getModalBeforeInChain().getModalId(), {
+            shopId: {
+                id: itemData.id,
+                name: itemData.name
+            }
         });
     }
 
-    /**
-     * @param {string} modalBeforeAttributeId
-     */
-    #setModalBefore(modalBeforeAttributeId) {
-        this.modalBeforeAttributeId = modalBeforeAttributeId;
-        this.backButtonTag.dataset.bsTarget = '#' + modalBeforeAttributeId;
-    }
-
-    /**
-     * @param {HTMLElement} itemTag
-     * @param {Event} event
-     */
-    handleMessageShopSelected(itemTag, event) {
-        const itemData = JSON.parse(itemTag.dataset.data);
-
-        this.#sendMessageShopsListShopSelected(itemData.id, itemData.name);
+    #openModalCreateShop() {
+        this.#modalManager.openNewModal('shop_create_modal');
     }
 
     handleMessageConnected() {
         this.#sendMessageInitializeToPaginatorContentLoaderJsComponent();
     }
+
 
     /**
      * @param {object} event
@@ -107,16 +167,10 @@ export default class extends Controller {
      * @param {object} event.detail.content
      * @param {boolean} event.detail.content.showedFirstTime
      * @param {HTMLElement} event.detail.content.triggerElement
-     * @param {object} event.detail.content.triggerElementData
-     * @param {string} event.detail.content.triggerElementData.modalBefore
-     * @param {string} event.detail.content.triggerElementData.controllerModalEventHandler
+     * @param {ModalManager} event.detail.content.modalManager
      */
     handleMessageBeforeShowed({ detail: { content } }) {
-        if (content.triggerElementData.modalBefore !== null) {
-            this.#setModalBefore(content.triggerElementData.modalBefore);
-        }
-
-        this.modalBeforeControllerEventHandler = content.triggerElementData.controllerModalEventHandler;
+        this.#modalManager = content.modalManager;
         this.#sendMessagePageChangeToPaginatorJsComponent(1);
     }
 
@@ -134,9 +188,9 @@ export default class extends Controller {
     }
 
     #sendMessageInitializeToPaginatorContentLoaderJsComponent() {
-        communication.sendMessageToChildController(this.paginatorContentLoaderJsComponent, 'initialize', {
+        communication.sendMessageToChildController(this.#paginatorContentLoaderJsComponent, 'initialize', {
             responseManageCallback: this.#responseManageCallback.bind(this),
-            postResponseManageCallback: this.#postResponseManageCallback.bind(this)
+            postResponseManageCallback: () => { }
         });
     }
 
@@ -144,7 +198,7 @@ export default class extends Controller {
      * @param {number} page
      */
     #sendMessagePageChangeToPaginatorJsComponent(page) {
-        communication.sendMessageToChildController(this.paginatorContentLoaderJsComponent, 'changePage', {
+        communication.sendMessageToChildController(this.#paginatorContentLoaderJsComponent, 'changePage', {
             page: page
         },
             'PaginatorJsComponent'
@@ -152,23 +206,10 @@ export default class extends Controller {
     }
 
     /**
-     * @param {string} shopId
-     * @param {string} shopName
-     */
-    #sendMessageShopsListShopSelected(shopId, shopName) {
-        communication.sendMessageToNotRelatedController(this.element, 'shopSelected', {
-            shopId: shopId,
-            shopName: shopName,
-        },
-            `[${this.element.dataset.controller}|>${this.modalBeforeControllerEventHandler}]`
-        );
-    }
-
-    /**
      * @param {number} pagesTotal
      */
     #sendMessagePagesTotalToPaginatorJsComponent(pagesTotal) {
-        communication.sendMessageToChildController(this.paginatorJsComponent, 'pagesTotal', {
+        communication.sendMessageToChildController(this.#paginatorJsComponent, 'pagesTotal', {
             pagesTotal: pagesTotal
         });
     }
