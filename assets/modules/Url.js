1ea2e656e4e6b8660632bf8ba0cae34ef0c82271
Victor <victorrmmb@hotmail.com>
2024-02-20 09:39:01 +0100

    refactor: :recycle: Added to Url.js functions getSecicion, getGroupName, and const SECTIONS

--- assets/modules/Url.js
+++ assets/modules/Url.js
@@ -1,3 +1,20 @@
 export function getLocale() {
     return window.location.pathname.split('/')[1];
 }
+
+/**
+ * @returns {string}
+ */
+export function getGroupName() {
+    return window.location.pathname.split('/')[2];
+}
+
+export function getSection() {
+    return window.location.pathname.split('/')[3];
+}
+
+export const SECTIONS = {
+    PRODUCT: 'product',
+    SHOP: 'shop',
+    ORDER: 'order'
+};
