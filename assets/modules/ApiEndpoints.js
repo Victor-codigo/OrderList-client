85caaab812a6afeccdae52b9aa0a952d3921ef5f
Victor <victorrmmb@hotmail.com>
2024-02-20 09:40:11 +0100

    refactor: :sparkles: Added enpoint getProductsData and getProductsNames

--- assets/modules/ApiEndpoints.js
+++ assets/modules/ApiEndpoints.js
@@ -1,9 +1,11 @@
 import * as fetch from 'App/modules/Fetch';
+import * as url from 'App/modules/Url';
 
 const API_VERSION = '1';
 const API_DOMAIN = 'http://orderlist.api';
 const CLIENT_DOMAIN = 'http://orderlist.client';
 const GET_SHOPS_URL = `${API_DOMAIN}/api/v${API_VERSION}/shops`;
+const GET_PRODUCTS_URL = `${API_DOMAIN}/api/v${API_VERSION}/products`;
 const POST_SHOP_URL = `${CLIENT_DOMAIN}/ajax/{locale}/{group_name}/shop/create`;
 
 
@@ -69,28 +71,52 @@ export async function getShopsNames(queryParameters) {
  * @returns {Promise<import('App/modules/Fetch').ResponseDto>}
  */
 export async function createShop(form, submitter) {
-    const url = POST_SHOP_URL
-        .replace('{locale}', getLocale())
-        .replace('{group_name}', getGroupName())
+    const createShopUrl = POST_SHOP_URL
+        .replace('{locale}', url.getLocale())
+        .replace('{group_name}', url.getGroupName())
 
-    form.action = url;
+    form.action = createShopUrl;
     const formData = new FormData(form, submitter);
-    const response = await fetch.createFormRequest(url, 'POST', formData, {});
+    const response = await fetch.createFormRequest(createShopUrl, 'POST', formData, {});
 
     return await response.json();
 }
 
 /**
- * @returns {string}
- */
-function getGroupName() {
-    return window.location.pathname.split('/')[2];
+ * @param {object} queryParameters see api documentation
+ * @returns {Promise<{
+*      'page': int,
+*      'pages_total': int,
+*      'products': array
+* }>}
+* @throws Error
+*/
+export async function getProductsData(queryParameters) {
+    const response = await fetch.createJsonRequest(GET_PRODUCTS_URL, 'GET', queryParameters);
+    const responseJson = await fetch.manageResponseJson(response,
+        (response) => {
+            return {
+                data: {
+                    page: 1,
+                    pages_total: 0,
+                    products: []
+                }
+            }
+        },
+        null
+    );
+
+    return responseJson.data;
 }
 
 /**
- * @returns {string}
- */
-function getLocale() {
-    return window.location.pathname.split('/')[1];
+* @param {object} queryParameters see api documentation
+* @returns string[]
+* @throws Error
+*/
+export async function getProductsNames(queryParameters) {
+    const responseData = await getProductsData(queryParameters);
+
+    return responseData.products.map((product) => product.name);
 }
 
