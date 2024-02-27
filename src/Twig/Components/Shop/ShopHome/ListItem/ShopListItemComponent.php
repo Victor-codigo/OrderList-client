b306b1146d7d301a2ec2c4c43ee2df23d328b762
Victor <victorrmmb@hotmail.com>
2024-02-27 16:21:37 +0100

    refactor: :recycle: Added support for itemInfo modal. Removed data shown in list item like created on and description

--- src/Twig/Components/Shop/ShopHome/ListItem/ShopListItemComponent.php
+++ src/Twig/Components/Shop/ShopHome/ListItem/ShopListItemComponent.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Twig\Components\Shop\ShopHome\ListItem;
 
+use App\Controller\Request\Response\ProductDataResponse;
+use App\Controller\Request\Response\ProductShopPriceDataResponse;
 use App\Twig\Components\HomeSection\HomeList\ListItem\HomeListItemComponent;
 use App\Twig\Components\HomeSection\HomeList\ListItem\HomeListItemComponentDto;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
@@ -14,6 +16,8 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 )]
 final class ShopListItemComponent extends HomeListItemComponent
 {
+    public readonly string $productDataJson;
+
     public static function getComponentName(): string
     {
         return 'ShopListItemComponent';
@@ -23,6 +27,7 @@ final class ShopListItemComponent extends HomeListItemComponent
     {
         $this->data = $data;
         $this->loadTranslation();
+        $this->productDataJson = $this->parseItemDataToJson($data);
     }
 
     protected function loadTranslation(): void
@@ -33,8 +38,45 @@ final class ShopListItemComponent extends HomeListItemComponent
             $this->translate('shop_modify_button.title'),
             $this->translate('shop_remove_button.alt'),
             $this->translate('shop_remove_button.title'),
+            $this->translate('shop_info_button.alt'),
+            $this->translate('shop_info_button.title'),
             $this->translate('shop_image.alt'),
             $this->translate('shop_image.title'),
         );
+    }
+
+    private function parseItemDataToJson(ShopListItemComponentDto $shopData): string
+    {
+        /** @var ProductShopPriceDataResponse[] $productShopsPricesDataByProductId */
+        $productShopsPricesDataByProductId = array_combine(
+            array_map(
+                fn (ProductShopPriceDataResponse $productShopPrice) => $productShopPrice->productId,
+                $shopData->productsShopsPrice
+            ),
+            $shopData->productsShopsPrice
+        );
+
+        $productShopsData = array_map(
+            fn (ProductDataResponse $productData) => [
+                'id' => $productData->id,
+                'name' => $productData->name,
+                'description' => $productData->description,
+                'image' => $productData->image,
+                'price' => $productShopsPricesDataByProductId[$productData->id]->price,
+                'unit' => $productShopsPricesDataByProductId[$productData->id]->unitMeasure,
+            ],
+            $shopData->products
+        );
+
+        $shopDataToParse = [
+            'id' => $shopData->id,
+            'name' => $shopData->name,
+            'description' => $shopData->description,
+            'image' => $shopData->image,
+            'createdOn' => $shopData->createdOn->format('Y-m-d'),
+            'itemsPrices' => $productShopsData,
+        ];
+
+        return json_encode($shopDataToParse, JSON_THROW_ON_ERROR);
     }
 }
--- src/Twig/Components/Shop/ShopHome/ListItem/ShopListItemComponentDto.php
+++ src/Twig/Components/Shop/ShopHome/ListItem/ShopListItemComponentDto.php
@@ -8,17 +8,30 @@ use App\Twig\Components\HomeSection\HomeList\ListItem\HomeListItemComponentDto;
 
 class ShopListItemComponentDto extends HomeListItemComponentDto
 {
+    /**
+     * @param array<{
+     *      id: string,
+     *      name: string,
+     *      description: string,
+     *      image: string,
+     *      price; float|null
+     * }> $shops
+     */
     public function __construct(
         public readonly string $componentName,
         public readonly string $id,
         public readonly string $name,
         public readonly string $modifyFormModalIdAttribute,
         public readonly string $deleteFormModalIdAttribute,
+        public readonly string $infoFormModalIdAttribute,
         public readonly string $translationDomainName,
 
         public readonly string|null $description,
         public readonly string|null $image,
         public readonly \DateTimeImmutable $createdOn,
+
+        public readonly array $products,
+        public readonly array $productsShopsPrice,
     ) {
     }
 }
