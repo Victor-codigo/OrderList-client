084ef0c578048b2d50f4421f4dfcce64150ac141
Victor <victorrmmb@hotmail.com>
2024-02-27 13:28:11 +0100

    refactor: :recycle: Changed MODAL_CHAINS, now, each modal has: name, modalId and open properties.

--- assets/Config.js
+++ assets/Config.js
@@ -2,21 +2,111 @@ export const MODAL_CHAINS = {
     productCreateChain: {
         name: 'productCreateChain',
         modals: {
-            productCreate: 'product_create_modal',
-            shopList: 'shop_list_select_modal',
-            shopCreate: 'shop_create_modal'
+            productCreate: {
+                name: 'productCreate',
+                modalId: 'product_create_modal',
+                open: {
+                    shopList: 'shop_list_select_modal'
+                }
+            },
+            shopList: {
+                name: 'shopList',
+                modalId: 'shop_list_select_modal',
+                open: {
+                    shopCreate: 'shop_create_modal'
+                }
+            },
+            shopCreate: {
+                name: 'shopCreate',
+                modalId: 'shop_create_modal',
+                open: {
+                    shopCreated: 'product_create_modal'
+                }
+            }
         }
     },
 
     productModifyChain: {
         name: 'productModifyChain',
         modals: {
-            productModify: 'product_modify_modal',
-            shopList: 'shop_list_select_modal',
-            shopCreate: 'shop_create_modal'
+            productModify: {
+                name: 'productModify',
+                modalId: 'product_modify_modal',
+                open: {
+                    shopList: 'shop_list_select_modal'
+                }
+            },
+            shopList: {
+                name: 'shopList',
+                modalId: 'shop_list_select_modal',
+                open: {
+                    shopCreate: 'shop_create_modal'
+                }
+            },
+            shopCreate: {
+                name: 'shopCreate',
+                modalId: 'shop_create_modal',
+                open: {
+                    shopCreated: 'product_modify_modal'
+                }
+            }
         }
-    }
-}
+    },
+
+    shopCreateChain: {
+        name: 'shopCreateChain',
+        modals: {
+            shopCreate: {
+                name: 'shopCreate',
+                modalId: 'shop_create_modal',
+                open: {
+                    productsListModal: 'product_list_select_modal'
+                }
+            },
+            productList: {
+                name: 'productList',
+                modalId: 'product_list_select_modal',
+                open: {
+                    productCreate: 'product_create_modal'
+                }
+            },
+            productCreate: {
+                name: 'productCreate',
+                modalId: 'product_create_modal',
+                open: {
+                    productCreated: 'shop_create_modal'
+                }
+            }
+        }
+    },
+
+    shopModifyChain: {
+        name: 'shopModifyChain',
+        modals: {
+            shopModify: {
+                name: 'shopModify',
+                modalId: 'shop_modify_modal',
+                open: {
+                    productsListModal: 'product_list_select_modal'
+                }
+            },
+            productList: {
+                name: 'productList',
+                modalId: 'product_list_select_modal',
+                open: {
+                    productCreate: 'product_create_modal'
+                }
+            },
+            productCreate: {
+                name: 'productCreate',
+                modalId: 'product_create_modal',
+                open: {
+                    productCreated: 'shop_modify_modal'
+                }
+            }
+        }
+    },
+};
 
 /**
  * @type {Intl.DateTimeFormatOptions}
@@ -39,11 +129,10 @@ export const CURRENCY = '€';
  * @property {string} description
  * @property {string} image
  * @property {string} createdOn
- * @property {ItemShopData[]} shops
+ * @property {ItemPriceData[]} itemsPrices
  */
-
 /**
- * @typedef {Object} ItemShopData
+ * @typedef {Object} ItemPriceData
  * @property {string} id
  * @property {string} name
  * @property {string} description
