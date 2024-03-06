ec896441f1b084bb5d48d2ee4eac1370b10038b1
Victor <victorrmmb@hotmail.com>
2024-03-06 08:22:30 +0100

    refactor: :recycle: Added constraint to ListOrdersCreateComponent date to buy input, not allow to select a date before now

--- src/Twig/Components/ListOrders/ListOrdersCreate/ListOrdersCreate_controller.js
+++ src/Twig/Components/ListOrders/ListOrdersCreate/ListOrdersCreate_controller.js
@@ -3,12 +3,20 @@ import * as form from 'App/modules/form';
 
 
 export default class extends Controller {
+    /**
+     * @type {HTMLInputElement}
+     */
+    #dateToBuyTag;
+
     connect() {
+        this.#dateToBuyTag = this.element.querySelector('[data-js-list-orders-date-to-buy]');
+        this.#dateToBuyTag.min = new Date().toISOString().slice(0, -8);
+
         this.formValidate();
     }
 
     formValidate() {
-        form.validate(this.element);
+        form.validate(this.element, null);
     }
 }
 
