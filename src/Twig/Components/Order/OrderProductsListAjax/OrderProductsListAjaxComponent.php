d37aead5d511454cde9234d807cba3470af94e18
Victor <victorrmmb@hotmail.com>
2024-03-14 13:21:13 +0100

    feat: :sparkles: Added component OrderProductsListAjaxComponent

---
+++ src/Twig/Components/Order/OrderProductsListAjax/OrderProductsListAjaxComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components\Order\OrderProductsListAjax;
+
+use App\Twig\Components\HomeSection\ItemsListAjax\ItemsListAjaxComponent;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(
+    name: 'OrderProductsListAjaxComponent',
+    template: 'Components/Order/OrderProductsListAjax/OrderProductsListAjaxComponent.html.twig'
+)]
+class OrderProductsListAjaxComponent extends ItemsListAjaxComponent
+{
+    public static function getComponentName(): string
+    {
+        return 'OrderProductsListAjaxComponent';
+    }
+
+    protected function loadTranslation(): void
+    {
+        $this->lang = new OrderProductsListAjaxComponentLangDto(
+            $this->translate('title'),
+            $this->translate('product_image.title'),
+            $this->translate('button_back.label'),
+            $this->translate('button_create_product.label'),
+        );
+    }
+}
---
+++ src/Twig/Components/Order/OrderProductsListAjax/OrderProductsListAjaxComponentDto.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components\Order\OrderProductsListAjax;
+
+use App\Twig\Components\HomeSection\ItemsListAjax\ItemsListAjaxComponentDto;
+
+class OrderProductsListAjaxComponentDto extends ItemsListAjaxComponentDto
+{
+}
