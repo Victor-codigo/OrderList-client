16687621722a832cd34169241bf1800e93b1090d
Victor <{GIT_EMAIL}>
2024-11-10 13:45:46 +0100

    refactor: :bug: Fixed OrderHomeSection_Controller.js fetch do not get the image

--- src/Twig/Components/Order/OrderHome/Home/OrderHomeSection_controller.js
+++ src/Twig/Components/Order/OrderHome/Home/OrderHomeSection_controller.js
@@ -3,10 +3,9 @@ import * as locale from 'App/modules/Locale';
 import * as endpoint from 'App/modules/ApiEndpoints';
 import * as communication from 'App/modules/ControllerCommunication';
 import * as bootstrap from 'bootstrap';
-import * as fetch from 'App/modules/Fetch'
 
 const SHARED_RESOURCE_PLACEHOLDER = '--shared_recourse_id--';
-const LOGO_URL = location.protocol + location.host + '/build/images/common/logo-white.png';
+const LOGO_URL = location.protocol + '//' + location.host + '/build/images/common/logo-white.png';
 
 export default class extends HomeSectionComponent {
 
@@ -91,7 +90,9 @@ export default class extends HomeSectionComponent {
         communication.sendMessageToChildController(this.#shareButton, 'showButton');
 
         if (responseData.status === 'ok') {
-            this.#share(responseData.data.list_orders_id);
+            await this.#share(responseData.data.list_orders_id);
+
+            return;
         }
 
         if (Object.hasOwn(responseData.errors, 'tryout_route_permissions')) {
@@ -111,7 +112,7 @@ export default class extends HomeSectionComponent {
      */
     async #share(sharedRecourseId) {
         try {
-            let shareIconResponse = await fetch.createRequest(LOGO_URL);
+            let shareIconResponse = await fetch(LOGO_URL);
             let shareIcon = new File(
                 [await shareIconResponse.blob()],
                 'logo.png',
